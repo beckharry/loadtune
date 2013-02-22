9ed3831436bdd8a37fe1396b84ee6b8aca42495f
Henry Wang <beckharry@gmail.com>
2013-02-22 03:15:06 -0500

    modification to files

--- Makefile
+++ Makefile
@@ -1,2 +1,7 @@
 all: loadtune.c
-	gcc -o loadtune -g loadtune.c
+	gcc -o loadtune-daemon -g loadtune.c -lpthread
+install:
+	@cp loadtune-daemon /bin/
+	@cp loadtune /bin/
+	@chmod +x /bin/loadtune
+	@echo "Install loadtune successfully."
--- loadtune.c
+++ loadtune.c
@@ -1,66 +1,109 @@
+#define _GNU_SOURCE
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
-#include <signal.h>
-#include <termios.h>
 #include <ctype.h>
 #include <sys/select.h>
-#include <sys/time.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <pthread.h>
-#include <math.h>
 
 #define DEFAULT_LOAD_TUNE 0.4
 #define LOADAVG_PROC "/proc/loadavg"
 #define lt_debug(fmt, args...) \
 	do { \
-		printf("[%s: %s, %d] " fmt, \
+		printf(">>>> [%s: %s, %d] " fmt, \
 				__FILE__, __FUNCTION__, __LINE__, ##args); \
 	} while(0)
+#undef lt_debug
+#define lt_debug
 
-static float get_first_float(FILE * fd)
+
+static float loadtune = 0;
+static int nrcpus = 0;
+static int affinity = 0;
+
+static float get_loadavg(void)
 {
+	static FILE * loadavg_fd = 0;
 	char * lineptr = NULL; /* TODO static lineptr to avoid 
 							  memory allocating and free, to improve 
 							  peroforamance */
 	size_t linelen = 0;
 	float first = 0;
 	char linedelim[] = " ";
-	getline(&lineptr, &linelen, fd);
+
+	loadavg_fd = fopen(LOADAVG_PROC, "r");
+	if (!loadavg_fd) {
+		printf("Failed to open %s.\n", LOADAVG_PROC);
+		return;
+	}
+
+	getline(&lineptr, &linelen, loadavg_fd);
 	if (lineptr) {
 		char * __lineptr = lineptr;
 		first = atof(strsep(&__lineptr, linedelim));
 		free(lineptr);
 	}
+
+	fclose(loadavg_fd);
+
 	return first;
 }
 
 static void print_help(void)
 {
-	int processor_cnt = 0;
-	processor_cnt = sysconf(_SC_NPROCESSORS_ONLN);
 	printf("Usage:\n");
-	printf("    loadtune target-value\n");
+	printf("    loadtune-daemon target-value\n");
 	printf("Options:\n");
 	printf("    For single core machine, target-value is between 0 and 1;\n");
 	printf("    For multiple[N] core machine, target-value is between 0 and N.\n");
-	printf("    (N is %d on this machine)\n", processor_cnt);
+	printf("    (N is %d on this machine)\n", nrcpus);
 	printf("Example:\n");
-	printf("    loadtune 0.4\n");
+	printf("    loadtune-daemon 0.4\n");
+}
+
+static void tune_proc(void * arg);
+static void tune_proc_start(void)
+{
+	pthread_t thread;
+	affinity++;
+	pthread_create(&thread, NULL, (void *)tune_proc, NULL);
+	pthread_join(thread, NULL);
+	return;
+}
+
+static void tune_proc(void * arg)
+{
+	pthread_t thread;
+	cpu_set_t cpuset;
+
+	lt_debug("affinity: %d.\n", affinity);
+	thread = pthread_self();
+	lt_debug("pthread: %d.\n", thread);
+	CPU_ZERO(&cpuset);
+	CPU_SET(affinity % nrcpus, &cpuset);
+	pthread_setaffinity_np(thread, sizeof(cpu_set_t), &cpuset);
+	while (get_loadavg() < loadtune){
+		lt_debug("pthread: %d.\n", thread);
+		lt_debug("current load average: %f.\n", get_loadavg());
+		lt_debug("target load average: %f.\n", loadtune);
+	}
+	lt_debug("end of tune_proc.\n");
+	return;
 }
 
 void main(int argc, char ** argv)
 {
-	float loadtune = 0;
-	FILE * loadavg_fd = 0;
 	float current_load = 0;
-	int processor_cnt = 0;
+	int proc_index = 0;
 
 	/* get processor cnt */
-	processor_cnt = sysconf(_SC_NPROCESSORS_ONLN);
-	lt_debug("processor_cnt: %d.\n", processor_cnt);
+	nrcpus = sysconf(_SC_NPROCESSORS_ONLN);
+	lt_debug("nrcpus: %d.\n", nrcpus);
+	lt_debug("pid: %d.\n", getpid());
 
 	/* parameters validation */
 	if (argc == 2) {
@@ -83,25 +126,23 @@ void main(int argc, char ** argv)
 		loadtune = atof(argv[1]);
 	}
 	else {
+		printf("Invalid parameter count.\n");
 		print_help();
-	}
-	lt_debug("loadtune: %f.\n", loadtune);
-	if (loadtune > (float)(processor_cnt)) {
-		printf("Invalid target-value, it MUST be lower than processor_cnt, see: loadtune --help\n");
-	}
-
-	/* get loadavg proc file handle */
-	loadavg_fd = fopen(LOADAVG_PROC, "r");
-	if (!loadavg_fd) {
-		printf("Failed to open %s.\n", LOADAVG_PROC);
 		return;
 	}
+	lt_debug("loadtune: %f.\n", loadtune);
+	if (loadtune > (float)(nrcpus)) {
+		printf("Invalid target-value, it MUST be lower than nrcpus.\n");
+		print_help();
+	}
 
-	current_load = get_first_float(loadavg_fd);
-	lt_debug("current_load: %f.\n", current_load);
-
-	/* release loadavg proc file handle */
-	fclose(loadavg_fd);
+	while(1) {
+		lt_debug("current load average: %f.\n", get_loadavg());
+		lt_debug("target load average: %f.\n", loadtune);
+		if (get_loadavg() < loadtune) {
+			tune_proc_start();
+		}
+	}
 
 	return;
 }
